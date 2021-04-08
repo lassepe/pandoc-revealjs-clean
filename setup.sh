ca6217238e7431e1ade88593d4a0ff9eb1bb260c
lassepe <lasse.peters@mailbox.org>
2021-04-08 16:02:15 +0200

    Setup script adds assets symlink to server dir

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 git submodule update --init --recursive
 ln -rsf ./slides.html ./assets/reveal.js/index.html
+ln -rs ./assets assets/reveal.js
 ln -rs ./media ./assets/reveal.js/
 
 npm install --prefix assets/reveal.js
