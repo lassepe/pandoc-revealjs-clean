6849fead2cdeb659437d18b37ea5d7812eff0696
lassepe <lasse.peters@mailbox.org>
2021-04-08 12:04:27 +0200

    Fix setup script bugs

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 git submodule update --init --recursive
-ln -s ./slides.html ./assets/reveal.js/
-ln -s ./media ./assets/reveal.js/
+ln -rsf ./slides.html ./assets/reveal.js/index.html
+ln -rs ./media ./assets/reveal.js/
 
 npm install --prefix assets/reveal.js
+npm audit fix --prefix assets/reveal.js
